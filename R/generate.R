d0e3b2680296065a07a07c584d6a6d56eee30518
Ada-Ciganlija <69331674+Ada-Ciganlija@users.noreply.github.com>
2020-12-15 21:07:02 -0500

    add initial files to project

---
+++ R/generate.R
@@ -0,0 +1,24 @@
+#' @export
+make_sonnet <- function(ref_text = NULL) {
+
+# TODO: Ensure incoming text is cleaned
+#       - extraneous whitespace is removed
+#       - clean up linebreak
+#       - lowercase all text
+#       - ensure that all unicode text is in correct encoding
+#       - remove any markup (e.g., HTML tags, etc.)
+
+# TODO: Analyze incoming text for sentiment
+#       - generate tokens from text
+#       - do stemming
+#       - generate graph model for poetic form
+
+# TODO: Get text resources
+#       - labeled data for syllabification
+#       - get sentiment corpus for all words
+#       - get word origin corpus (labelled with first usage years)
+#       - get word types, classifications (multiple labels per word)
+
+
+}
+
